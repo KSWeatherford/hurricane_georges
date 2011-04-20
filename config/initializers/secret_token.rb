db7996255514bc9627aff6dbeb1abe79b049dd85
Francis Pedraza <francis@doband.org>
2011-04-19 23:22:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HurricaneGeorges::Application.config.secret_token = '32a75f6b3b42602ade4f3b877ca982f1657d357bbb0804b794529281258e967867be2227ea82f7e8637621bfcc6158be8e1bedd56202ccaf34f88a5f18f443b9'
